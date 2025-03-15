64ce96dbe00ec2803bcfeac169091ad02a27b429
Karla Jazmin Ponce Guido <karlapg04@hotmail.com>
2025-03-15 06:27:28 +0000

    Defined is_adjacent and its helper functions.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,11 +1,53 @@
 #include "ladder.h"
-#include <queue>
-#include <vector>
 
 void error(string word1, string word2, string msg){
     cerr << word1 << msg << word2;
 }
 
+bool same_distance(string word1, string word2){
+    for (int i = 0; i < word1.length(); i++){
+            word1[i] = '_' ; word1[i] = '_';
+            if (word1 == word2) return true;
+    }
+    return false;
+}
+
+auto small_string = [](const string& str1, const string& str2){return str1.length() < str2.length();};
+
+bool add_letter(string smaller_string, string bigger_string, int d){
+    for (int i = 0; i < d; i++){
+        smaller_string.insert(i, 1, '_'); bigger_string[i] = '_';
+        if (smaller_string == bigger_string) return true;
+    }
+    return false;
+}
+
+bool remove_letter(string smaller_string, string bigger_string, int d){
+    for (int i = 0; i < d; i++){
+        bigger_string.erase(i, 1);
+        if (smaller_string == bigger_string) return true;
+    }
+    return false;
+}
+
+bool edit_distance_within(const std::string &str1, const std::string &str2, int d){
+    string smaller_string, bigger_string;
+    if (small_string(str1, str2)) smaller_string = str1, bigger_string = str2;
+    else  smaller_string = str2, bigger_string = str1;
+    if ((bigger_string.length()-smaller_string.length()) > 1) return false;
+    if (add_letter(smaller_string, bigger_string, d)) return true;
+    if (remove_letter(smaller_string, bigger_string, d)) return true;
+    return false;
+}
+
+bool is_adjacent(const string &word1, const string &word2){
+    if (word1 == word2) return false;
+    if (word1.length() == word2.length()) return same_distance(word1, word2);
+    else{
+        int d = (word1.length() > word2.length()) ? word1.length() : word2.length();
+        return edit_distance_within(word1, word2, d);}
+    return false;
+}
 
 vector<string> generate_word_ladder(const string &begin_word, const string &end_word, const set<string> &word_list){
     vector<string> partial_ladder;
