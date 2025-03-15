b5c9baec9bb91a74a47519e57b74c2ac39734022
Karla Jazmin Ponce Guido <karlapg04@hotmail.com>
2025-03-15 06:47:19 +0000

    Defined verify_word_ladder.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,4 +1,5 @@
 #include "ladder.h"
+#include <cassert>
 
 void error(string word1, string word2, string msg){
     cerr << word1 << msg << word2;
@@ -90,3 +91,15 @@ void print_word_ladder(const vector<string> &ladder){
     cout << endl;
 }
 
+void verify_word_ladder(){
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    vector<string> ladder = generate_word_ladder("wow", "woven", word_list);
+    print_word_ladder(ladder);
+}
